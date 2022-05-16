c8adcd0e750316b343db1a111f66cae02933342c
James Montemagno <james.montemagno@gmail.com>
2022-05-16 16:56:24 -0700

    remove tap from code behind

--- Finish/MonkeyFinder/View/MainPage.xaml.cs
+++ Finish/MonkeyFinder/View/MainPage.xaml.cs
@@ -7,18 +7,5 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 		BindingContext = viewModel;
 	}
-
-    private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
-    {
-		var monkey = ((VisualElement)sender).BindingContext as Monkey;        
-        
-		if (monkey == null)
-			return;
-
-		await Shell.Current.GoToAsync(nameof(DetailsPage), true, new Dictionary<string, object>
-		{
-			{"Monkey", monkey }
-		});
-	}
 }
 
