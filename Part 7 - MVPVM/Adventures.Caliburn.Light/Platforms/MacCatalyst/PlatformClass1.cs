3484837a3863e9638297d31e62cdfda7a4a27b6e
Bill Kratochvil <10552946+BillKrat@users.noreply.github.com>
2022-07-18 17:08:06 -0500

    Fixed build issues on Windows box

--- Part 7 - MVPVM/Adventures.Caliburn.Light/Platforms/MacCatalyst/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Adventures.Caliburn.Light;
-
-// All the code in this file is only included on Mac Catalyst.
-public class PlatformClass1
-{
-}
-
--- Part 7 - MVPVM/Adventures.Caliburn.Light/Platforms/Windows/PlatformClass1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Adventures.Caliburn.Light;
-
-// All the code in this file is only included on Windows.
-public class PlatformClass1
-{
-}
-
