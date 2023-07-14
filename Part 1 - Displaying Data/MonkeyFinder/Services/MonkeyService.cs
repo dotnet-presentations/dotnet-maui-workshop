2cf6bc53f942b9d56a66704cb9f76b44e11347fa
dvoron89 <dmitry.s.voron@gmail.com>
2023-07-14 15:45:30 +0300

    Session 1 im progress
    Stopped at 1:51:05

--- Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,30 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    HttpClient httpClient;
+    public MonkeyService()
+    {
+        httpClient = new HttpClient();
+    }
+
+    List<Monkey> monkeyList = new();
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList.Count > 0)
+            return monkeyList;
+
+        var url = "https://montemagno.com/monkeys.json";
+
+        var response = await httpClient.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+
+        return monkeyList;
+    }
 }
