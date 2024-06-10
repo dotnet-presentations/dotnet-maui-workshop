71a22a97eec85695455ab61c05805a0dd6dd0a16
Maddy Montaquila <maddyleger1@gmail.com>
2024-06-10 11:18:13 +0200

    slide updates and deleting old junk

--- Part5Test/MonkeyFinder/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-﻿global using CommunityToolkit.Mvvm.ComponentModel;
-global using CommunityToolkit.Mvvm.Input;
-global using MonkeyFinder.Model;
-global using MonkeyFinder.ViewModel;
-global using System.Collections.ObjectModel;
-global using System.ComponentModel;
-global using System.Diagnostics;
-global using System.Runtime.CompilerServices;
-global using System.Text.Json;
