dcbbd10ef9913e9dd02612da0132ebca49304b57
Mille Elfver <mille.elfver98@gmail.com>
2024-06-27 07:46:13 +0200

    Added monkeyservice to monkeyviewmodel

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,9 +1,40 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+
+namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
-    public MonkeysViewModel()
+    MonkeyService _monkeyService;
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+    public MonkeysViewModel(MonkeyService monkeyService)
     {
+        _monkeyService = monkeyService;
         Title = "Monkey Finder";
+    }
+
+    async Task GetMonkeysAsync()
+    {
+        if(IsBusy)
+            return;
+
+        try
+        {
+            IsBusy = true;
+            var monkeys = await _monkeyService.GetMonkeys();
+
+            if (Monkeys.Count != 0)
+                Monkeys.Clear();
+            
+            foreach (var monkey in monkeys)
+                Monkeys.Add(monkey);
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine($"Error: {e}");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
     }
 }
